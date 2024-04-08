3f760ceaa37049b920805ca8b1b28fb09fb0a976
eclipse1228 <qudtnrh@naver.com>
2024-04-08 15:10:46 +0900

    [chart.js] build with .css path on '/' -ByeongSu

--- chat.js
+++ chat.js
@@ -1,27 +1,39 @@
 const fs = require('fs');
 const http = require('http');
 const WebSocket = require('ws');
+const path = require('path');
 
-// Create an HTTP server // 
-const server = http.createServer((req, res) => {
-  // Serve index.html file (localhost://8080)
-  if (req.url === '/' || req.url === '/index.html') {
-    fs.readFile('index.html', (err, data) => {
+/* Create an HTTP server // -ByeongSuGo 24/04/08*/
+const server = http.createServer((req, res) => {                // Serve index.html file (localhost://8080)
+
+  let filePath = '.' + req.url;                                 // File Path Configuration 
+  if (filePath == './') {
+    filePath = './index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase(); // file's extension (ex file.txt -> extname is 'txt') 
+  let contentType = 'text/html';                                // Default is html 
+  switch (extname) {                                            // Browser autoMatically jenerate Request for additional files (javascripts , css files ..) but We have to set ContentType.
+    case '.css':
+      contentType = 'text/css';                                 
+      break;
+  }
+
+    fs.readFile(filePath, (err, data) => {
       if (err) {
         res.writeHead(500);
-        return res.end('Error loading index.html');
+        return res.end('Error loading file');
       }
-      res.writeHead(200);
-      res.end(data); // using data(index.html's data) // response body 
-    });
-  }
+      res.writeHead(200, {'Content-Type':contentType});         
+      res.end(data, 'utf-8');                                   // using data(index.html's data) // response body 
+      });
 });
 
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   console.log('WebSocket connection established');
-  ws.on('message', (message) => {                   // once message comes to server,
+  ws.on('message', (message) => {                   // Once message comes to server,
     console.log('received: %s', message);
     // Broadcast incoming message to all clients
     wss.clients.forEach((client) => {
